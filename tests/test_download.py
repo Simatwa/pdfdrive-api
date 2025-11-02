085b0b7cc81067faf801981f07d85e59b4fc3856
smartwa <smartwacaleb@gmail.com>
2025-11-02 04:37:40 +0300

    feat: Add Session class to BookDetails constructor

--- tests/test_download.py
+++ tests/test_download.py
@@ -2,20 +2,21 @@ import pytest
 
 from pdfdrive_api.download import Downloader
 from pdfdrive_api.extras import BookDetails, BookPageModel
+from pdfdrive_api.requests import Session
 
 link = "https://pdfdrive.com.co/rich-dad-poor-dad-pdf/"
 
 
 @pytest.mark.asyncio
 async def test_book_details_extraction():
-    book_details = BookDetails(link)
+    book_details = BookDetails(link, session=Session())
     details = await book_details.get_details(for_download=True)
     assert isinstance(details, BookPageModel)
 
 
 @pytest.mark.asyncio
 async def test_book_download():
-    book_details = BookDetails(link)
+    book_details = BookDetails(link, session=Session())
     downloader = Downloader()
 
     details = await book_details.get_details(for_download=True)
